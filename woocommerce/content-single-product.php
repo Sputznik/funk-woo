0af263eaa2f34598f75a0f9791761f5bcc9daee7
Samuel Thomas <samvthom16@gmail.com>
2018-11-28 12:42:25 +0530

    [#1] - show tabs

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -104,7 +104,7 @@ if ( $shop_single['thumbs-pos'] === 'vertical' ) {
 		 * @hooked woocommerce_upsell_display - 15
 		 * @hooked woocommerce_output_related_products - 20
 		 */
-		//do_action( 'woocommerce_after_single_product_summary' );
+		do_action( 'woocommerce_after_single_product_summary' );
 
 	?>
 
