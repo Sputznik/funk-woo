22fa7d7ebd0fe65ca50c4d58ceb570716a29ff55
guneetnarula <guneetnarula@sputznik.com>
2018-11-28 14:46:25 +0530

    styles update 2.1 and length-weight function

--- functions.php
+++ functions.php
@@ -12,11 +12,11 @@ function martivi_child_enqueue_scripts() {
     wp_enqueue_style( 'martivi-child', get_stylesheet_uri() );
 }
 
-add_filter('woocommerce_variable_price_html', 'single_variation_price', 10, 2); 
+add_filter('woocommerce_variable_price_html', 'single_variation_price', 10, 2);
 
-function single_variation_price( $price, $product ) { 
+function single_variation_price( $price, $product ) {
      $price = '';
-     $price .= wc_price($product->get_price()); 
+     $price .= wc_price($product->get_price());
      return $price;
 }
 
@@ -26,11 +26,33 @@ remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_pr
 // INCLUDE TABS JUST BELOW THE PRODUCT SUMMARY
 add_action( 'woocommerce_single_product_summary', 'funk_product_attributes', 25 );
 
-
+//redo
 function funk_product_attributes(){
 	global $product;
 	$length = $product->get_attribute( 'length' );
 	$weight = $product->get_attribute( 'weight' );
 	echo $length;
 	echo $weight;
+  ?>
+    <div class="row wc-length-weight">
+      <div class="col-sm-4">Length:</div>
+      <div class="col-sm-8">
+        <ul>
+          <?php
+            for ($i,$i < 5,$i++) {
+              if ($i < $length) {
+              ?>
+                <li class="full-circle"></li>
+              <?php
+              } else {
+                ?>
+                  <li class="empty-circle"></li>
+                <?php
+              }
+            }
+          ?>
+        </ul>
+      </div>
+    </div>
+  <?php
 }
